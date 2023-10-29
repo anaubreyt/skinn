691b706d60f28a2f74a79a535b2cc75b3e99deeb
anaubreyt <144357466+anaubreyt@users.noreply.github.com>
2023-10-29 22:20:56 +0300

    Add files via upload

---
+++ script/slider.js
@@ -0,0 +1,13 @@
+new Swiper ('.swiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    simulateTouch: false,
+    slidesPerView: 'auto',
+    loop: true,
+    spaceBetween: 1,
+    centeredSlides: true,
+    initialSlide: 1
+});
+
